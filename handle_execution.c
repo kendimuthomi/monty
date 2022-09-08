f6e10fe22a6edda860eabbb573f80b9a9f9f3f80
kendimuthomi <kendijackline42@gmail.com>
2022-09-08 09:23:57 +0000

    Figuring this out

--- handle_execution.c
+++ handle_execution.c
@@ -1,5 +1,6 @@
+#define _GNU_SOURCE
 #include "monty.h"
-#include <string.h>
+#include <stdio.h>
 
 void free_tokens(void);
 unsigned int token_arr_len(void);
@@ -161,7 +162,7 @@ int run_monty(FILE *script_fd)
 	if (line && *line == 0)
 	{
 		free(line);
-		return (malloc_error());
+		return (mem_error());
 	}
 
 	free(line);
